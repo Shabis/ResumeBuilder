b0db152d1efe1791f93a8b3781c76b5bc49e7164
Shelby Clayton and Stephen Emery <shelby@>
2016-09-13 14:32:55 -0700

    add functionality to ui.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,38 +3,41 @@ import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
+import java.util.ArrayList;
 
 public class App {
   public static void main(String[] args) {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
-    // get("/", (request, response) -> {
-    //   Map<String, Object> model = new HashMap<String, Object>();
-    //   model.put("template", "templates/hello.vtl" );
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
-    // get("/favorite_photos", (request, response) -> {
-    //   Map<String, Object> model = new HashMap<String, Object>();
-    //   model.put("template", "templates/favorite_photos.vtl" );
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
-    // get("/form", (request, response) -> {
-    //   Map<String, Object> model = new HashMap<String, Object>();
-    //   model.put("template", "templates/form.vtl");
-    //   return new ModelAndView(model, "templates/layout.vtl");
-    // }, new VelocityTemplateEngine());
-    //
-    // get("/greeting_card", (request, response) -> {
-    //   Map<String, Object> model = new HashMap<String, Object>();
-    //   String recipient = request.queryParams("recipient");
-    //   String sender = request.queryParams("sender");
-    //   model.put("recipient", recipient);
-    //   model.put("sender", sender);
-    //   model.put("template", "templates/greeting_card.vtl");
-    //   return new ModelAndView(model, "templates/layout.vtl");
-    // }, new VelocityTemplateEngine());
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("jobs", request.session().attribute("jobs"));
+      model.put("template", "templates/index.vtl" );
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/jobs", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+
+      ArrayList<Job> jobs = request.session().attribute("jobs");
+      if (jobs == null) {
+        jobs = new ArrayList<Job>();
+        request.session().attribute("jobs", jobs);
+      }
+
+
+      String position = request.queryParams("position");
+      String company = request.queryParams("company");
+      String location = request.queryParams("location");
+      String description = request.queryParams("description");
+      String start = request.queryParams("start");
+      String end = request.queryParams("end");
+      Job userJob = new Job (position, company, location, description, start, end);
+      jobs.add(userJob);
+
+      model.put("template", "templates/success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
