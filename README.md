3ba93574e20bdcf8d7eb1d3fab3df4ae5a6a3fec
Shelby Clayton and Stephen Emery <stephen@>
2016-09-13 13:27:40 -0700

    add constructor and get commands.

---
+++ README.md
@@ -0,0 +1,23 @@
+## Project Name
+Resume Builder
+
+## Features
+This program will take user input and create a resume for them.
+
+##Technologies
+Java, JUnit, Gradle, Spark, Velocity Engine
+
+## Usage
+To use the code, you can clone the repository at: [https://github.com/mehequanna/java-resumebuilder](https://github.com/mehequanna/java-resumebuilder).
+If this were a program:
+* $ git clone https://github.com/mehequanna/java-resumebuilder
+* $ cd java-resumebuilder
+* $ gradle run
+* Go to localhost:4567 in a browser.
+
+## Author/s
+Shelby Clayton & Stephen Emery
+
+##License
+This work can be used under the MIT License.
+Copyright (c) 2016 Shelby Clayton & Stephen Emery
---
+++ src/main/java/Resume.java
@@ -0,0 +1,36 @@
+public class Job {
+ String mPostion;
+ String mCompany;
+ String mLocation;
+ String mDescription;
+ String mStart;
+ String mEnd;
+
+public Job(String position, String company, String location, String description, String start, String end) {
+  mPostion = position;
+  mCompany = company;
+  mLocation = location;
+  mDescription = description;
+  mStart = start;
+  mEnd = end;
+}
+public String getPosition() {
+  return mPosition;
+}
+public String getCompany() {
+  return mCompany;
+}
+public String getLocation() {
+  return mLocation;
+}
+public String getDescription() {
+  return mDescription;
+}
+public String getStart() {
+  return mStart;
+}
+public String getEnd() {
+  return mEnd;
+}
+
+}
